f438197636e54ea3810498e9e3c89a6a57f017a1
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-01-06 15:50:48 +0900

    Refactored attribute to an interface.

--- DataReusable/src/app/shared/models/attribute.ts
+++ DataReusable/src/app/shared/models/attribute.ts
@@ -1,27 +1,10 @@
 export type AttributeType = 'string' | 'number';
 export const ATTRIBUTE_TYPES = ['string', 'number'];
 
-interface IAttribute{
+/* Convenience interface used to manage the definition of the attributes a
+ * given element */
+export interface Attribute{
+	name: string;
 	type: AttributeType;
 	unique: boolean;
-}
-
-/**
- * Convenience class used to manage the definition of the attributes an element
- * in the data model contains
- */
-export class Attribute implements IAttribute {
-	protected _type: AttributeType;
-	protected _unique: boolean;
-
-	constructor(type: AttributeType, unique: boolean = false){
-		this._type = type;
-		this._unique = unique;
-	}
-
-	get type(): AttributeType { return this._type; }
-	set type(t: AttributeType) { this._type = t; }
-	
-	get unique(): boolean { return this._unique; }
-	set unique(u: boolean) { this._unique = u; }
 }
