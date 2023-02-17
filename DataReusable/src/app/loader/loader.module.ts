a51f190c09dc029f1506e3041c7e30f288732cf7
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-02-17 10:11:03 +0900

    Initial skeleton for upload tab

--- DataReusable/src/app/loader/loader.module.ts
+++ DataReusable/src/app/loader/loader.module.ts
@@ -6,10 +6,12 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { SharedModule } from '../shared/shared.module';
+import { UploadItemDialogComponent } from './upload-item-dialog/upload-item-dialog.component';
 
 @NgModule({
   declarations: [
 		DataLoaderComponent,
+  UploadItemDialogComponent,
 	],
   imports: [
     CommonModule,
---
+++ DataReusable/src/app/loader/upload-item-dialog/upload-item-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-item-dialog',
+  templateUrl: './upload-item-dialog.component.html',
+  styleUrls: ['./upload-item-dialog.component.css']
+})
+export class UploadItemDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
