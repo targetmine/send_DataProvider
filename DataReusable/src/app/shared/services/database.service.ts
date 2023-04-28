f7c3aec8d0b54d2939183317d4719968ac5fa2c8
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2023-04-28 16:22:08 +0900

    Refactoring of database service for handling of relation requests. Bugs fixed.

--- DataReusable/src/app/shared/services/database.service.ts
+++ DataReusable/src/app/shared/services/database.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Element } from 'src/app/shared/models/element';
+import { Attribute } from 'src/app/shared/models/attribute';
 import { Relation } from 'src/app/shared/models/relation';
 import { firstValueFrom } from 'rxjs';
 import { ShareModelService } from './share-model.service';
@@ -46,69 +47,71 @@ export class DatabaseService {
 	}
 
 	public addRelation(relation: Relation): Promise<HttpResponse<Object>>{
-		let url = `${environment.serverURL}/provider/relation/${relation.name}`;
 		const eles = this.modelService.elements.getValue();
-		let body: any = {	};
 		switch (relation.cardinality){
-			case 'one to one':
-				body.element = relation.srcElement;
-				body.columns = []
-				eles
-					.filter(e => e.name === relation.trgElement)
-					.map(e => {
-						e.attributes.forEach(a => {
-							if(a.unique)
-								body.columns.push({name: `${e.name}_${a.name}`, type: a.type});
-						});
-					});
-				return firstValueFrom(this.http.post(url, body, {
+			case 'one to one': {
+				const url = `${environment.serverURL}/provider/element/add-column/`;
+				const columns = eles
+					.filter (e => e.name === relation.trgElement)
+					.reduce((e,c) => [...e, ...c.attributes], [] as Attribute[])
+					.filter(a => a.unique)
+					.map(a => { return {name: `${relation.trgElement}_${a.name}`, type: a.type} })
+					;
+				const body = {
+					name: relation.srcElement,
+					columns
+				};
+				return firstValueFrom(this.http.put(url, body, {
 					headers: {'Content-type': 'application/json'},
 					observe: 'response'
 				}));
-			case 'one to many':
-				body.element = relation.trgElement;
-				body.columns = [];
-				eles
-					.filter(e => e.name === relation.srcElement)
-					.map(e => {
-						e.attributes.forEach(a => {
-							if(a.unique)
-								body.columns.push({name: `${e.name}_${a.name}`, type: a.type})
-						});
-					});
-				return firstValueFrom(this.http.post(url, body, {
+			}
+			case 'one to many': {
+				const url = `${environment.serverURL}/provider/element/add-column/`;
+				const columns = eles
+					.filter (e => e.name === relation.srcElement)
+					.reduce((e,c) => [...e, ...c.attributes], [] as Attribute[])
+					.filter(a => a.unique)
+					.map(a => {return {name: `${relation.srcElement}_${a.name}`, type: a.type} })
+					;
+				const body = {
+					name: relation.trgElement,
+					columns
+				};return firstValueFrom(this.http.put(url, body, {
 					headers: {'Content-type': 'application/json'},
 					observe: 'response'
 				}));
-			default: //'many to many'
-				url = `${environment.serverURL}/provider/element/`;
-				body.name = `${relation.srcElement}${relation.srcAttribute}${relation.trgElement}${relation.trgAttribute}`;
-				body.columns = [];
-				body.primaryKeys = [];
+			}
+			case 'many to many': {
+				const url = `${environment.serverURL}/provider/element/`;
+				let columns: any[] = [];
+				let primaryKeys: any[] = [];
 				eles
-					.filter(e => e.name === relation.srcElement || e.name === relation.trgElement)
-					.map(e => {
-						e.attributes.forEach(a => {
-							if (a.unique){
-								body.columns.push(`${e.name}_${a.name} ${a.type}`);
-								body.primaryKeys.push(`${e.name}_${a.name}`);
-							}
-						});
+					.filter (e => e.name === relation.srcElement)
+					.reduce((e,c) => [...e, ...c.attributes], [] as Attribute[])
+					.filter(a => a.unique)
+					.forEach(a => {
+						columns.push(`src_${a.name} ${a.type}`);
+						primaryKeys.push(`src_${a.name}`);
 					});
-				return firstValueFrom(this.http.post(url, body, {
+				eles
+					.filter (e => e.name === relation.trgElement)
+					.reduce((e,c) => [...e, ...c.attributes], [] as Attribute[])
+					.filter(a => a.unique)
+					.forEach(a => {
+						columns.push(`trg_${a.name} ${a.type}`);
+						primaryKeys.push(`trg_${a.name}`);
+					});
+				const body = {
+					name: `${relation.srcElement}_${relation.trgElement}`,
+					primaryKeys: primaryKeys,
+					columns: columns
+				};return firstValueFrom(this.http.post(url, body, {
 					headers: {'Content-type': 'application/json'},
 					observe: 'response'
 				}));
+			}
 		}
-	}
-
-	public addRelationOneToOne(table: string, columns: any[]){
-		const url = `${environment.serverURL}/provider/relation/one-to-one/`;
-		const body = { table, columns };
-		return firstValueFrom(this.http.post(url, body, {
-			headers: { 'Content-type': 'application/json'},
-			observe: 'response'
-		}));
 	}
 
 	public uploadElement(element: string, primaryKeys: string[], columns: string[], data: any){
